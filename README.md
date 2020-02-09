856b9428b923ab06c761f4b33ec99882b8d591ce
huangjingyin <940849738@qq.com>
2020-02-09 15:29:00 +0800

    add sql ddl

--- README.md
+++ README.md
@@ -6,6 +6,21 @@ https://spring.io/guides/gs/serving-web-content/
 https://elasticsearch.cn/explore
 https://v3.bootcss.com/getting-started/
 https://developer.github.com/apps/
+https://docs.spring.io/spring-boot/docs/2.2.4.RELEASE/reference/html/spring-boot-features.html#boot-features-sql
 
 ##工具
 https://git-scm.com/downloads
+
+##脚本
+```sql
+create table community.user
+(
+	id int auto_increment primary key,
+	account_id varchar(100) null,
+	name varchar(50) null,
+	token char(36) null,
+	gmt_create bigint null,
+	gmt_modified bigint null
+);
+
+```
--- src/main/java/life/mangjiang/community/controller/AuthorizeController.java
+++ src/main/java/life/mangjiang/community/controller/AuthorizeController.java
@@ -2,13 +2,20 @@ package life.mangjiang.community.controller;
 
 import life.mangjiang.community.dto.AccessTokenDTO;
 import life.mangjiang.community.dto.GithubUser;
+import life.mangjiang.community.mapper.UserMapper;
+import life.mangjiang.community.model.User;
 import life.mangjiang.community.provider.GithubProvider;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -20,9 +27,13 @@ public class AuthorizeController {
     private String clientSecret;
     @Value("${github.redirect.uri}")
     private String redirectUri;
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
-    public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name = "state")String state){
+    public <request> String callback(@RequestParam(name="code") String code,
+                                     @RequestParam(name = "state")String state,
+                                   HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirectUri);
@@ -30,8 +41,24 @@ public class AuthorizeController {
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+
+        if (githubUser!=null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+
+//            登陆成功，写cookie和session
+            request.getSession().setAttribute("githubUser",githubUser);
+            return "redirect:/";
+
+        }else{
+//            登陆失败，重新登
+            return "redirect:/";
+        }
     }
 }
